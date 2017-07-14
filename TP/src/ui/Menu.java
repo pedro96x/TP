fa25aa19d2002197d64c4286f757cbc0bffe2ad6
gui2_ <gui2_@LAPTOP-9CMT2CQQ.mshome.net>
2017-07-14 18:44:06 -0300

    Habia commitiado sin las clases nuevas creo que ahora si va a estar bien

---
+++ TP/src/ui/Menu.java
@@ -0,0 +1,46 @@
+package ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class Menu extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu frame = new Menu();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Menu() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Vamos bien");
+		lblNewLabel.setBounds(70, 43, 193, 70);
+		contentPane.add(lblNewLabel);
+	}
+}
