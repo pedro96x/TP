096409c46f9fbb45bc8daae7f10e2f3abf85bad3
Pedro <Pedro@DESKTOP-4840A09>
2017-07-23 12:25:53 -0300

    Cambios en CtrlReserva. No puedo eliminar los autos repetidos, pero igual lo subo porque tendria que andar

--- TP/src/controladores/CtrlReserva.java
+++ TP/src/controladores/CtrlReserva.java
@@ -1,6 +1,7 @@
 package controladores;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import data.DataReserva;
 import entidades.Auto;
@@ -21,6 +22,53 @@ public class CtrlReserva {
 //		Auto auto;
 		ArrayList <Auto> autos = controladorAuto.getAutosByID(tipoauto.getId());
 		ArrayList <Reserva> reservas = dataRes.getReservas();
+		ArrayList <Auto> autos2 = new ArrayList <Auto>();
+		
+		
+		for (Reserva reserva : reservas) {
+			for (Auto auto : autos) {
+				if (!NuevoDate.seSolapan(reserva.getFechaIni(), reserva.getFechaFin(), fechaInicioRes, fechaFinRes)){
+					autos2.add(auto);
+				}
+			}
+		}//Esto revisa reserva por reserva a ver si el auto se solapa, si no es asi lo agrega a autos2
+		//El problema es que un auto puede ser usado por dos reservas sin que se solape las fechas 
+		//Ejemplo: reserva 1 fechaInicio = "2017-10-10" fechaFin = "2017-10-15" 
+		//         reserva 2 fechaInicio = "2017-10-16" fechaFin = "2017-10-20" 
+		//Si pido un auto para fechaInicio = "2017-10-23" y fechaFin "2017-10-27" el auto va a ser agregado dos veces a autos2
+		// porque el for anterior verifica que no se solape con la reserva 1 y agrega el auto a auto2, despues hace lo mismo con reserva 2  y agrega el amismo auto
+		
+		//Ahora vamos a intentar eliminar los repetidos
+		//Como no nos funca el remove() voy a hacer otro arreglo mas.
+		
+		
+		//foma 1: (la cree yo, pero no funciona, devuelve un arrayList vacio)
+		ArrayList <Auto> autos3 = new ArrayList <Auto>();
+		
+		for (Auto auto2 : autos2) {
+			boolean encontrado= false;
+			
+			for (Auto auto3 : autos3) {
+				if(autos3.isEmpty()){
+					autos3.add(auto2);
+				}
+				if (auto2.getId()==auto3.getId()){
+					encontrado = true;
+					break;
+				}
+				if (auto2.getId()!=auto3.getId()){
+					encontrado = false;
+				}
+				
+			}
+			if (encontrado = false){
+				autos3.add(auto2);
+			}
+		}
+		
+		//forma 2: (sacada de internet)
+//		autos2 = new ArrayList <Auto>(new HashSet<Auto>(autos2));
+		
 		
 //		for (int i = 0; i < autos.size(); i++) {
 //			
@@ -57,6 +105,6 @@ public class CtrlReserva {
 //		}
 	
 	
-	return autos;
+	return autos3;
 	}
 }
